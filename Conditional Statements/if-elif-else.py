797728b3d682120ae23d81bd4e86f01d820f7f90
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-26 11:11:03 +0530

    Conditional Statements

---
+++ Conditional Statements/if-elif-else.py
@@ -0,0 +1,12 @@
+num=int(input("Enter number : "))
+
+if(num<0):
+    print("Number Entered is Negative")
+elif(num==0):
+    print("Number Entered is Zero")
+elif(num>999):
+    print("Number is Greater than 999")
+else:
+    print("Number Entered is Positive")
+
+print("End of Program")
