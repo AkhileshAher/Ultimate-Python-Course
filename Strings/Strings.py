fe03bd983254187c9599051263883db903ca38d9
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-26 09:05:06 +0530

    Create Strings.py

---
+++ Strings/Strings.py
@@ -0,0 +1,13 @@
+name="Akhilesh"
+apple="""He said , Lets eat an apple having more than 2 kilograms
+in weight Also said you are fools"""
+
+print("Hello",name)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+# print(name[5])
+for character in apple:
+    print(character)
