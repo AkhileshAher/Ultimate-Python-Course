18017e2c0732e0cac564937af272d50320cb8b73
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-26 11:05:42 +0530

    If-else Conditionals

---
+++ Conditional Statements/if-else.py
@@ -0,0 +1,9 @@
+age=int(input("Enter number of age : "))
+
+if(age<=18):
+    print("You cannot Drive !!!")
+    print("Sorry , Stay Safe")
+
+else:
+    print("You can Drive")
+    print("Yay!!!!")
