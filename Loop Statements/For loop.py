bb9679453f3d90a5fd9902c19d1740934a310f5d
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-26 17:11:22 +0530

    Loops in Python

---
+++ Loop Statements/For loop.py
@@ -0,0 +1,18 @@
+colors=["Red","Orange","Yellow","Blue","Green"]
+
+for color in colors:
+    print(color)
+    for i in color:
+        print(i)
+
+for i in range(20):
+    print(i+1)
+
+for i in range(0,200,3):
+    print(i)
+
+name = 'Akhilesh'
+for i in name:
+  print(i,end=" , ")
+  if(i =="b"):
+    print("This is something special!")
