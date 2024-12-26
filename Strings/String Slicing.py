3001d76669475f2c87212ef913b2a66e7290ffe7
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-26 09:41:53 +0530

    Create String Slicing.py

---
+++ Strings/String Slicing.py
@@ -0,0 +1,11 @@
+fruit="Watermelon"
+
+print(len(fruit))
+
+print(fruit[0:9])
+print(fruit[1:-1])
+print(fruit[:9])
+print(fruit[-1:len(fruit) - 3])
+print(fruit[:])
+print(fruit[-3:-1])
+print(fruit[-1:-3])
