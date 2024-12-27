f871e82fee7962afc41bc7321b0e7e27a5a659a4
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-27 17:13:09 +0530

    Create list.py

---
+++ Lists/list.py
@@ -0,0 +1,15 @@
+lst1=[3,5,6,"Harry","Sidddu",9,7,True,False]
+print(lst1)
+
+print(lst1[1:5])
+print(lst1[1:-4])
+print(lst1[0:10:2])
+
+if "6" in lst1:
+  print("Yes")
+else:
+  print("No")
+
+# Same thing applies for strings as well!
+if "Ha" in "Harry":
+  print("Yes")
