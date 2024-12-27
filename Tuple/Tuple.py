0318b364ba4256ee5d2a6000e736d7b287e7ec13
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-27 20:21:52 +0530

    Tuple in Python

---
+++ Tuple/Tuple.py
@@ -0,0 +1,13 @@
+tup = (3,5,9,"Akhilesh",True,102,'Values')
+
+print(tup)
+
+tup1=tup[3:-1]
+print(tup1)
+
+if "Akhilesh" in tup:
+    print("Yes It is Present in Tuple")
+else:
+    print("No It is Absent")
+
+print(tup[1:-2])
