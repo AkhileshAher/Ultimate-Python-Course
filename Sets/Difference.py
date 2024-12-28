43398debd730ab2f68c137acf2bed5662be8c133
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-28 09:20:08 +0530

    Set Methods

--- Sets/Difference.py
+++ Sets/Difference.py
@@ -2,3 +2,7 @@ cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
 cities2 = {"Seoul", "Kabul", "Delhi"}
 cities3 = cities.difference(cities2)
 print(cities3)
+
+cities = {"Tokyo", "Madrid", "Berlin", "Delhi"}
+cities2 = {"Seoul", "Kabul", "Delhi"}
+print(cities.difference(cities2))
