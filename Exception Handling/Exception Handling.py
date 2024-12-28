52da904d054a27ebf5fa6c858c2bad3d1e710412
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-28 22:41:29 +0530

    Exception Handling in Python

---
+++ Exception Handling/Exception Handling.py
@@ -0,0 +1,11 @@
+a=input("Enter the Number : ")
+print(f"Multiplication table of {a} is : ")
+
+try:
+    for i in range(1,11):
+        print(f"{int(a)} x {i} = {int(a) * i}")
+except:
+    print("Invalid Input")
+
+print("Some imp Lines of Code")
+print("End of the Program")
