c5e3133680838fae24613247a6d96ce7effd0260
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-29 17:50:58 +0530

    Import in Python

---
+++ Import/Import working.py
@@ -0,0 +1,21 @@
+import math
+res=math.sqrt(9) * math.pi
+print(res)
+
+from math import pi,sqrt as s
+resultant=s(9) * pi
+print(resultant)
+
+import math as m
+resu=m.sqrt(9) * m.pi
+print(resu)
+
+from math import sqrt,pi
+result=sqrt(9) * pi
+print(result)
+
+from docstrings import  square as sq
+sq(3)
+
+print(dir(math))
+print(math.nan, type(math.nan))
