67ca8cd8dc97014ba73aa44fe4640be84785fa4c
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-29 16:32:35 +0530

    Short Hand If Else in Python

---
+++ Short Hand If Else/Short hand if-else.py
@@ -0,0 +1,7 @@
+a=int(input("Enter an Integer : "))
+b=int(input("Enter an Integer : "))
+print("A") if a>b else print("B") if (b>a) else print("Default") 
+# Execution is as it checks condition and executes left statement to condition
+
+
+print("Hello Sir , A") if(a>b) else print("Hello Sir B")
