ea7850ce13441799abce090ad475fc7831247e45
Akhilesh Aher <aherakhilesh88@gmail.com>
2024-12-30 16:49:10 +0530

    Lambda Functions

---
+++ Functions/lambda.py
@@ -0,0 +1,12 @@
+# def double(x):
+#     return x*2
+
+# lambda arguments : expression
+
+double = lambda x : x*2
+square = lambda x : x*x
+cube = lambda x : x**3
+
+print(double(8))
+print(square(5))
+print(cube(3))
