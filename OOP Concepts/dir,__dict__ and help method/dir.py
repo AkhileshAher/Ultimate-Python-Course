40665bb5d5cee894d2ea9b1ce390a3bb848758bd
Akhilesh Aher <aherakhilesh88@gmail.com>
2025-01-02 20:51:12 +0530

    Object Oriented Programming

---
+++ OOP Concepts/dir,__dict__ and help method/dir.py
@@ -0,0 +1,8 @@
+x= (1,5,6)
+print(dir(x))
+
+
+y = [2, 5 ,7]
+print(dir(y))
+
+'''dir(): The dir() function returns a list of all the attributes and methods (including dunder methods) available for an object. It is a useful tool for discovering what you can do with an object.'''
