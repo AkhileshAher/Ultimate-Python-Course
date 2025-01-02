713250a61f06da92ba39414cec086787498fec88
Akhilesh Aher <aherakhilesh88@gmail.com>
2025-01-02 20:56:13 +0530

    Object Oriented Programming

---
+++ OOP Concepts/dir,__dict__ and help method/help.py
@@ -0,0 +1,11 @@
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+
+person = Person("John Snow",30)
+
+help(Person)
+help(str)
+
+'''The help() function is used to get help documentation for an object, including a description of its attributes and methods.'''
