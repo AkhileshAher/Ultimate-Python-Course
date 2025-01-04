5934bca069d08cdda4a684c2dbea8e33d9a5c704
Akhilesh Aher <aherakhilesh88@gmail.com>
2025-01-04 23:35:31 +0530

    Multiple Inheritance in Python

---
+++ OOP Concepts/Inheritance/multipleinheritance.py
@@ -0,0 +1,28 @@
+# MULTIPLE INHERITANCE
+
+
+class Employee:
+    def __init__(self,name):
+        self.name = name
+
+    def show(self):
+        print(f"The name is {self.name}")
+
+class Dancer:
+    def __init__(self,dance):
+        self.dance = dance
+
+    def show(self):
+        print(f"The dance is {self.dance}")
+
+class DancerEmployee(Dancer,Employee):
+    def __init__(self,name,dance):
+        self.dance = dance
+        self.name = name
+
+
+o =  DancerEmployee("Shivani","Kathak")
+print(o.name)
+print(o.dance)
+o.show()
+print(DancerEmployee.mro())  # Method Resolution Order
