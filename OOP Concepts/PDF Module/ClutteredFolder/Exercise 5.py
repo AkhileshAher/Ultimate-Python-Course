497e508834b4ccb8d0361f758a9d00cd70c78293
Akhilesh Aher <aherakhilesh88@gmail.com>
2025-01-05 13:45:14 +0530

    Delete OOP Concepts/PDF Module/ClutteredFolder directory

--- OOP Concepts/PDF Module/ClutteredFolder/Exercise 5.py
+++
@@ -1,9 +0,0 @@
-from pypdf import PdfWriter
-
-merger = PdfWriter()
-
-for pdf in ["OOPs Concept/ClutteredFolder/file1.pdf", "OOPs Concept/ClutteredFolder/file2.pdf", "OOPs Concept/ClutteredFolder/file3.pdf"]:
-    merger.append(pdf)
-
-merger.write("merged-pdf.pdf")
-merger.close()
