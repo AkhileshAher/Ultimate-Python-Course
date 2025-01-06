531ac9d557d8cb1276a858eae24d93daf9982dd3
Akhilesh Aher <aherakhilesh88@gmail.com>
2025-01-06 10:40:04 +0530

    Walrus Operator in Python'

---
+++ Walrus Operator/Walrus Operator.py
@@ -0,0 +1,14 @@
+# USING SIMPLE OPERATORS
+foods = list()
+while True:
+    food= input("What food do you like : ")
+    if(food=="quit"):
+        break
+    foods.append(food)
+
+
+
+# Using Walrus Operator
+foods = list()
+while (food:=input("What food do you like : "))!="quit":
+    foods.append(food)
